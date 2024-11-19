8377814fc750e2045848fe1bb36f2860d7861998
RobabEjaz <ejazrafay4@gmail.com>
2024-11-19 03:39:22 -0800

    Add files via upload

---
+++ multipication.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int  main()
+{
+    int a,b;
+    cout<<"Enter 1st num:";
+    cin>>a;
+    cout<<"Enter 2st num:";
+    cin>>b;
+    cout<<"Answer is:"<<a*b<<endl;
+ }
