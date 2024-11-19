d06823e62efea66104036571a45b9b5c345bcd3e
RobabEjaz <ejazrafay4@gmail.com>
2024-11-19 03:59:54 -0800

    Add files via upload

---
+++ if.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int  main()
+{
+    int a;
+    cout<<"Enter a num:";
+    cin>>a;
+    if(a<10)
+    {
+      cout<<"Your number is less then 10";
+    }
+    cout<<"Your number is greater then 10";
+ }
