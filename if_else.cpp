501417b8347b929fd7e4271dd0004fc3c0a88296
RobabEjaz <ejazrafay4@gmail.com>
2024-11-21 06:55:20 -0800

    Add files via upload

---
+++ if_else.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ int age;
+ cout<<"Enter your age:";
+ cin>>age;
+ if(age<18)
+ {
+    cout<<"You are in teen age!!";
+ }
+ else 
+ cout<<"You are adult!!";
+}
